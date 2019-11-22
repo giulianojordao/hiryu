a5212c39b34dffc93c5d192116ce988c22d4af9d
StefanYohansson <sy.fen0@gmail.com>
2019-11-21 23:02:29 -0300

    WIP: with postgres function

--- hiryu-engine/app/Main.hs
+++ hiryu-engine/app/Main.hs
@@ -4,15 +4,16 @@ import           Control.Monad.IO.Class         ( liftIO )
 import           Web.Scotty
 --import           Resolvers.Sheet.Api            ( sheetApi )
 import           Resolvers.Campaign.Api         ( campaignApi )
-import           Database.Connection            ( inHandlerDb, doMigrations, doSeeds )
+import           Database.Connection            ( withPostgres, doMigrations, doSeeds )
 import           Network.Wai.Middleware.RequestLogger ( logStdoutDev )
 
 importGQLDocumentWithNamespace "schema.gql"
 
 main :: IO ()
 main = do
-  inHandlerDb doMigrations
-  inHandlerDb doSeeds
+  withPostgres $ do
+    doMigrations
+    doSeeds
   scotty 3000 $ do
     middleware logStdoutDev
 --    post "/sheet" $ raw =<< (liftIO . sheetApi =<< body)
--- hiryu-engine/src/Database/Connection.hs
+++ hiryu-engine/src/Database/Connection.hs
@@ -7,14 +7,16 @@
 {-# LANGUAGE QuasiQuotes                #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
-module Database.Connection (inHandlerDb, doMigrations, doSeeds) where
+module Database.Connection (withPostgres, doMigrations, doSeeds) where
 
 import           Control.Monad.IO.Class  (liftIO)
 import           Database.Persist
 import           Database.Persist.Postgresql
 import           Database.Persist.TH
 import           Database.Persist.Sql
-import           Control.Monad.Logger    (runStderrLoggingT)
+import           Control.Monad.Logger    (runStderrLoggingT, runNoLoggingT, NoLoggingT)
+import           Control.Monad.Trans.Reader (ReaderT)
+import           Control.Monad.Trans.Resource (ResourceT)
 import           Data.Password
 import           Conduit
 import           Entities.User
@@ -25,11 +27,9 @@ connStr = "host=localhost dbname=test user=test password=test port=5432"
 salt :: Salt
 salt = Salt "as13h398h013xmc40tc2"
 
-inHandlerDb = liftIO . dbFunction
-
-dbFunction query = runStderrLoggingT $ 
-        withPostgresqlPool connStr 10 $
-        \pool -> liftSqlPersistMPool query pool
+withPostgres :: ReaderT SqlBackend (NoLoggingT (ResourceT IO)) a -> IO a
+withPostgres =
+  runNoLoggingT . withPostgresqlPool connStr 10 . liftSqlPersistMPool
 
 doMigrations = do
   runMigration $ migrateUser
