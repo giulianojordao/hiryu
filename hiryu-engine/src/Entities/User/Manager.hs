b2d5952a6c66ee9b26d3b1d8c0ccc4a21e18ff21
StefanYohansson <sy.fen0@gmail.com>
2019-11-23 18:51:59 -0300

    cleanup Entities.User.Manager

--- hiryu-engine/src/Entities/User/Manager.hs
+++ hiryu-engine/src/Entities/User/Manager.hs
@@ -7,19 +7,17 @@
 {-# LANGUAGE QuasiQuotes                #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE StandaloneDeriving         #-}
 module Entities.User.Manager where
 
-import           Control.Monad.IO.Class  (liftIO, MonadIO)
+import           Control.Monad.IO.Class        (MonadIO)
+import           Database.Connection           (inHandlerDb, fromInt, Mod)
 import           Database.Persist
 import           Database.Persist.Postgresql
 import           Database.Persist.TH
 import           Data.Text
 import           Data.Int
-import           Control.Monad.Trans.Resource (ResourceT)
-import           Control.Monad.Logger    (runNoLoggingT, NoLoggingT)
-import           Database.Connection    (inHandlerDb, fromInt, Mod)
 import           Database.Model
+
 
 getUser :: MonadIO m => Int64 -> Mod m (Maybe User)
 getUser = get . fromInt
