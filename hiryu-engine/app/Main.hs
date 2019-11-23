df8f4d63d948955bc34e62dae231a1ebcbc6aed3
StefanYohansson <sy.fen0@gmail.com>
2019-11-23 18:45:22 -0300

    cleanup Main

--- hiryu-engine/app/Main.hs
+++ hiryu-engine/app/Main.hs
@@ -1,28 +1,16 @@
-{-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DerivingStrategies    #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NamedFieldPuns        #-}
 {-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeFamilies          #-}
-
 module Main where
 
-import           Control.Monad.IO.Class         ( liftIO )
+import           Control.Monad.IO.Class               (liftIO)
 import           Web.Scotty
---import           Resolvers.Sheet.Api            ( sheetApi )
-import           Resolvers.Campaign.Api         ( campaignApi )
-import           Database.Persist
-import           Database.Persist.Postgresql
-import           Database.Persist.TH
-import           Database.Connection            ( inHandlerDb )
-import           Database.Model                 ( doMigrations, doSeeds )
+--import           Resolvers.Sheet.Api                ( sheetApi )
+import           Resolvers.Campaign.Api               ( campaignApi )
+import           Database.Connection                  ( inHandlerDb )
+import           Database.Model                       ( doMigrations, doSeeds )
 import           Entities.User.Manager
 import           Network.Wai.Middleware.RequestLogger ( logStdoutDev )
-import           Data.Text                  (Text)
+
 
 main :: IO ()
 main = do
